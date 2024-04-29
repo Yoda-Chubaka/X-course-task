5c7b4c24d5518920531f80684609d88ee14726cf
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-04-29 16:54:34 +0300

    Update

--- js/index.js
+++ js/index.js
@@ -12,3 +12,23 @@
     refs.menu.classList.toggle("is-open");
   }
 })();
+
+
+
+
+
+var minus = document.getElementById('minus');
+var plus = document.getElementById('plus');
+var value = document.getElementById('book-quantity');
+
+function buttonMinus() {
+  // console.log("kjh");
+  value.stepDown();
+}
+
+
+function buttonPlus() {
+  // console.log("kjh");
+  value.stepUp();
+}
+
