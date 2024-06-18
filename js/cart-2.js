0c75d045e65e95812c78efb2d59e4a68225842fd
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-06-18 15:08:05 +0300

    Updated

---
+++ js/cart-2.js
@@ -0,0 +1,52 @@
+import booksData from '../data/books.json' with { type: "json" };
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cartMainElement = document.getElementById("container-cart");
+    const addButton = document.querySelector(".add-button");
+
+    // if (cartMainElement) {
+    //     addButton.addEventListener("click", function () {
+            
+const quantityOfBooks = document.getElementById("book-quantity").value;
+            const totalPriceValue = document.getElementById("total-price").textContent;
+            if (quantityOfBooks > 0) {
+                
+                cartMainElement.insertAdjacentHTML("beforeend", `
+                    <section class="section-vertical-align-full">
+                        <div>
+                            <div class="purshase-button-box">
+                                <button class="active-purshase-button link-purshase">Purсhase</button>
+                            </div>
+                            <div class="box-full-cart">
+                                <ul id="book-item-cart" class="full-item-cart">
+                                    <li>${booksData.books[0].title}</li>
+                                    <li>Items: ${quantityOfBooks}</li>
+                                    <li>${booksData.books[0].price}</li>
+                                </ul>
+                            </div>
+                            <div class="total-price-box">
+                                <p>Total price, $ ${totalPriceValue}</p>
+                            </div>
+                        </div>
+                    </section>
+                `);
+            } else {
+                cartMainElement.insertAdjacentHTML("beforeend", `
+                    <section class="section-vertical-align">
+                        <div class="empty-box">
+                            <div class="purshase-button-box">
+                                <button class="inactive-purshase-button link-purshase">Purсhase</button>
+                            </div>
+                            <img src="./images/cart-big-3.png" class="invert-2" width="200px" alt="empty card" />
+                            <p class="paragraph-empty-cart">Your cart is empty...</p>
+                            <p class="paragraph-empty-cart-grey">Add your favorite books to cart</p>
+                        </div>
+                    </section>
+                `);
+            }
+        });
+//     } else {
+//         console.error("Required elements not found");
+//     }
+// });
--- js/cart.js
+++ js/cart.js
@@ -1,49 +1,19 @@
-import booksData from '../data/books.json' with { type: "json" };
+document.addEventListener("DOMContentLoaded", function () {
+    const addButton = document.querySelector(".add-button");
+    const boxForNotifications = document.getElementById("purchase-notification");
+    const quantityOfBooks = document.getElementById("book-quantity").value;
 
-const cartMainElement = document.getElementById("container-cart");
-const addButton = document.querySelector(".add-button");
-
-addButton.addEventListener("onclick", function () {
-    if (onClick) {
-        booksData.books.forEach(book => {
-            const quantityOfBooks = document.getElementById("book-quantity").target.value;
-            const totalPriceValue = document.getElementById("total-price").target.value;
-            cartMainElement.innerHTML = `
-                <section class="section-vertical-align-full">
-                    <div>
-                    <div class="purshase-button-box">
-                        <button class="active-purshase-button link-purshase">Purсhase
-                        </button>
-                    </div>
-                    <div class="box-full-cart">
-                        <ul id="book-item-cart" class="full-item-cart">
-                            <li>${book.title}</li>
-                            <li>Items: ${quantityOfBooks}</li>
-                            <li>${book.price}</li>
-                        </ul>
-                    </div>
-                    <div class="total-price-box">
-                        <p>Total price, $ ${totalPriceValue}</p>
-                    </div>
-                </div>
-            </section>
-        `
+    if (addButton) {
+        addButton.addEventListener("click", function () {
+            boxForNotifications.textContent = "Book has been added successfully!";
+            boxForNotifications.classList.add("purchase-notification");
+            setTimeout(() => {
+                boxForNotifications.classList.remove("purchase-notification");
+                boxForNotifications.classList.add("purchase-inactive");
+            }, 2000);
         });
-} else {
-    cartMainElement.innerHTML = `
-        <section class="section-vertical-align">
-            <div class="empty-box">
-                <div class="purshase-button-box">
-                    <button class="inactive-purshase-button link-purshase">Purсhase
-                    </button>
-                </div>
-                <img src="./images/cart-big-3.png" class="invert-2" width="200px" alt="empty card" />
-                <p class="paragraph-empty-cart">Your cart is empty...</p>
-                <p class="paragraph-empty-cart-grey">Add your favorite books to cart</p>
-            </div>
-        </section>
-    `
-}
-})
-
-
+    }    
+    else {
+            console.error("Required elements not found");
+        }
+});
